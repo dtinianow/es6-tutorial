6a8668e9c43b7559ffbf21ae6b749809357e7745
David Tinianow <david.tinianow@gmail.com>
2017-01-16 14:29:50 -0700

    Setup babel

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,8 +1,10 @@
-var calculateMonthlyPayment = function (principal, years, rate) {
+'use strict';
+
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
     if (rate) {
         var monthlyRate = rate / 100 / 12;
     }
-    var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
     return monthlyPayment;
 };
 
